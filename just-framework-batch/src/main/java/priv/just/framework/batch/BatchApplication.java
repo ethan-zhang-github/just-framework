61d3b600846689e8a0450c3cc9bccf75870e40f9
JUST1984 <yixiezi1994@gmail.com>
2020-03-17 17:13:13 +0800

    update

---
+++ just-framework-batch/src/main/java/priv/just/framework/batch/BatchApplication.java
@@ -0,0 +1,18 @@
+package priv.just.framework.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: yixiezi1994@gmail.com
+ * @date: 2020-03-12 14:21
+ */
+@SpringBootApplication
+public class BatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+
+}
