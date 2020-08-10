a03a567f7851a28e636a06ec5c3ea5e88a16c493
JUST1984 <yixiezi1994@gmail.com>
2020-08-10 20:43:17 +0800

    solutino

---
+++ just-framework-algorithm/src/main/java/priv/just/framework/algorithm/midoftwoarrays/Solution.java
@@ -0,0 +1,14 @@
+package priv.just.framework.algorithm.midoftwoarrays;
+
+/**
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2 。
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ * @see <a href="https://mp.weixin.qq.com/s/FBlH7o-ssj_iMEPLcvsY2w"></a>
+ * @author ethan
+ */
+public interface Solution {
+
+    int invoke(int[] arr1, int[] arr2);
+
+}
